2fdf8c7ff555e40f91b289205fc1bffd60d5f5e9
Jesko7 <jjputzi0410@gmail.com>
2026-05-04 00:51:43 +0200

    KLj

--- crates/manager/src/run.rs
+++ crates/manager/src/run.rs
@@ -2,5 +2,9 @@ use crate::read_file;
 use rython_to_ir;
 
 pub fn run(file_name: &str) {
-    let content = read_file::read_file(file_name);
+    let content = read_file::read_file(file_name).unwrap();
+    let tokens = rython_to_ir::lexer::Lexer::create_tokens(content);
+    let mut parser = rython_to_ir::parser::Parser::new(tokens);
+    let ast = parser.parse();
+    dbg!(ast);
 }
