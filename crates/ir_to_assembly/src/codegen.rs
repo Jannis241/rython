6008547ac89eded207a95278c040cfe882009710
Jesko7 <jjputzi0410@gmail.com>
2026-05-04 16:58:59 +0200

    ka

--- crates/ir_to_assembly/src/codegen.rs
+++ crates/ir_to_assembly/src/codegen.rs
@@ -9,22 +9,29 @@ macro_rules! emit {
 
 pub struct AsmCodeGen {
     out: String,
+    input: IrModule,
 }
 
 impl AsmCodeGen {
     fn gen_asm(input: IrModule) -> String {
-        let mut asm_code_gen = AsmCodeGen { out: String::new() };
+        let mut asm_code_gen = AsmCodeGen {
+            out: String::new(),
+            input,
+        };
 
         asm_code_gen.generate_globals();
         asm_code_gen.generate_constants();
-        asm_code_gen.generate_fucntions();
+        asm_code_gen.generate_functions();
 
         asm_code_gen.out
     }
 
-    fn generate_fucntions(&mut self) {
-        emit!(self, "label:");
+    fn generate_functions(&mut self) {
+        for function in self.input.functions.clone() {
+            self.generate_function(function);
+        }
     }
+    fn generate_function(&mut self, function: IrFunction) {}
     fn generate_globals(&mut self) {}
     fn generate_constants(&mut self) {}
 }
