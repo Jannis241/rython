4770d13953d99d7fdcfa816fd37c562c047498b8
Jannis <jannis2401p@gmail.com>
2026-05-04 18:07:55 +0200

    update

--- crates/manager/src/run.rs
+++ crates/manager/src/run.rs
@@ -1,5 +1,5 @@
 use crate::read_file;
-use rython_to_ir;
+use rython_to_ir::{self, codegen};
 
 pub fn run(file_name: &str) {
     let content = read_file::read_file(file_name).unwrap();
@@ -7,5 +7,17 @@ pub fn run(file_name: &str) {
     dbg!(&tokens);
     let mut parser = rython_to_ir::parser::Parser::new(tokens);
     let ast = parser.parse();
-    let _ = dbg!(ast);
+    let _ = dbg!(&ast);
+    match ast {
+        Ok(tree) => {
+            let module = codegen::generate_code(&tree);
+
+            println!("Generated IR Module: {:?}", module);
+
+        }
+        Err(e) => {
+            eprintln!("Parsing Error: {:?}", e);
+            return;
+        }
+    }
 }
